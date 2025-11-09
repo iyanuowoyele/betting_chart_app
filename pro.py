9dbd87a32d98c5a32c4a77f29b27337d037b4ef0
owoyeleiyanu <owoyeleiyanuoluwapo@gmail.com>
2025-11-10 00:10:55 +0100

    candlestick completed

--- pro.py
+++ pro.py
@@ -3,6 +3,14 @@ import pandas as pd
 import json as json
 import matplotlib.pyplot as mplot
 import streamlit as st 
+import plotly.graph_objects as gobj
+import random
+
+
+
+
+
+
 
 #loading my json data 
 with open('data.json','r') as json_data:
@@ -14,15 +22,19 @@ games_info = data['player_stats'][0]['games_info']
 
 #we now convert to dataframe 
 df = pd.DataFrame(games_info)
+
 #convert to date time for easy manipulation and sorting 
 df['game_date'] = pd.to_datetime(df['game_date'])
 
-st.title('player performance dashboard') # streamlit dashboard title 
+# streamlit dashboard title and dropdown selections 
+st.title('player performance dashboard') 
+chart_type = st.radio('chart type:',['line graph','candlesticks'])
 selected_stat = st.selectbox('choose a stat',['points','assists','rebounds'])
-fig,axes = mplot.subplots()
-#plotting our values 
 
 
+
+
+# assign a color to each stat
 set_color = None
 if selected_stat == 'points':
     set_color = 'red'
@@ -31,25 +43,45 @@ elif selected_stat == 'assists':
 else:
     set_color = 'yellow'       
 
+#plotting our values - for candlestick pattern
+df['open'] = df[selected_stat].shift(1).fillna(df[selected_stat])
+df['close'] = df[selected_stat]
+df['high'] = df[['open','close']].max(axis= 1) +[random.uniform(0.5,1.5) for _ in range (len(df))]
+df['low'] = df[['open','close']].min(axis= 1) -[random.uniform(0.5,1.5) for _ in range (len(df))]
 
 
+if chart_type == 'line graph':
 
-axes.plot(df['game_date'],df[selected_stat],marker = 'o', label = selected_stat,color = set_color)
-axes.set_title(f'Chart showing {player_name} performance')
-axes.set_xlabel('GAME DATE')
-axes.set_ylabel('STAT VALUES')
-axes.grid(True)
-axes.legend()
-st.pyplot(fig)
+#plotting our values - line chart section
+    fig,axes = mplot.subplots()
+    axes.plot(df['game_date'],df[selected_stat],marker = 'o', label = selected_stat,color = set_color)
+    axes.set_title(f'Chart showing {player_name} performance')
+    axes.set_xlabel('GAME DATE')
+    axes.set_ylabel('STAT VALUES')
+    axes.grid(True)
+    axes.legend()
+    st.pyplot(fig)
+else :
+#create a candlestick figure 
+    fig = gobj.Figure(data=[gobj.Candlestick(
+    x = df['game_date'],
+    open = df['open'],
+    close = df['close'],
+    high = df['high'],
+    low = df['low'],
+    increasing_line_color='green',
+    decreasing_line_color='red',
+    name = selected_stat
+)])    
 
 
+    fig.update_layout (
+        title = f'Chart showing {player_name} performance',
+        xaxis_title="Game Date",
+        yaxis_title="Stat Value",
+        template = 'plotly_white',
+        xaxis_rangeslider_visible = False
+    )
+    fig.update()
 
-# print(df)
-
-#load and preview the data 
-# lf = pd.read_csv('month.csv')
-# lf = pd.read_json('data.json')
-# print(lf.head())
-
-
-# mplot.plot(lf[''])
+    st.plotly_chart(fig) 
